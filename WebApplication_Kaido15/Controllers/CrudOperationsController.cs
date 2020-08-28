d2c25665036e59c5abc88ac62455ac4fa1ff9bbd
海藤 一臣 <qaswr.cdwef.8642@icloud.com>
2020-08-28 17:11:10 +0900

    支店で60%進んだ

---
+++ WebApplication_Kaido15/Controllers/CrudOperationsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity.Core.Objects;
+using WebApplication_Kaido15.Class;
+
+namespace WebApplication_Kaido15.Controllers
+{
+    public class CrudOperationsController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult Index(Models.Student student, int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new Entities())
+                {
+                    ObjectParameter result = new ObjectParameter("result", typeof(String));
+                    ObjectParameter createdId = new ObjectParameter("createdId", typeof(String));
+                    if (id == null)
+                        context.sp_InsertStudent(student.name, student.address, student.age, student.standard, student.percent, student.status, result, createdId);
+                    else
+                        context.sp_UpdateStudent(id, student.name, student.address, student.age, student.standard, student.percent, student.status, result);
+
+                    TempData["Result"] = createdId.Value == null ? result.Value : result.Value + " New Student Id is " + createdId.Value;
+                }
+            }
+            ViewBag.Operation = id == null ? "Add Student" : "Update Student";
+            BindStudent(0);
+            return View();
+        }
+
+        public ActionResult Index(int? id, int? page)
+        {
+            BindStudent(Convert.ToInt32(page));
+            Models.Student mStudent = new Models.Student();
+            if (id != null)
+            {
+                using (var context = new Entities())
+                {
+                    var result = context.sp_GetStudentById(id);
+                    var targetList = result.Select(x => new Models.Student() { id = x.Id, name = x.Name, address = x.Address, age = x.Age, standard = x.Standard, percent = x.Percent, addedOn = x.AddedOn, status = x.Status }).ToList();
+                    mStudent = targetList.ToList().FirstOrDefault();
+                    ViewBag.Operation = "Update Student";
+                    return View(mStudent);
+                }
+            }
+            else
+                ViewBag.Operation = "Insert Student";
+            return View();
+        }
+
+        public void BindStudent(int page)
+        {
+            using (var context = new Entities())
+            {
+                int pageSize = 4;
+                int pageNo = page == 0 ? 1 : page;
+
+                PagingInfo pagingInfo = new PagingInfo();
+                pagingInfo.CurrentPage = pageNo;
+                pagingInfo.TotalItems = context.Student.Count();
+                pagingInfo.ItemsPerPage = pageSize;
+                ViewBag.Paging = pagingInfo;
+
+                List<Models.Student> mStudentList = new List<Models.Student>();
+                var result = context.sp_GetStudent(pageNo, pageSize);
+                var targetList = result.Select(x => new Models.Student() { id = x.Id, name = x.Name, address = x.Address, age = x.Age, standard = x.Standard, percent = x.Percent, addedOn = x.AddedOn, status = x.Status }).ToList();
+                mStudentList = targetList.ToList();
+                ViewBag.StudentList = mStudentList;
+            }
+        }
+    }
+
+}
---
+++ WebApplication_Kaido15/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Kaido15.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        public string address { get; set; }
+
+        [Required]
+        public int age { get; set; }
+
+        [Required]
+        public string standard { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d+\.\d{0,2}$", ErrorMessage = "Invalid should be like 79.76")]
+        public decimal percent { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime addedOn { get; set; }
+
+        [Required]
+        public bool status { get; set; }
+    }
+}
